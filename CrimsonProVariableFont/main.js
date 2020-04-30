9d1f5f4d78da771c1c42779b2a188d00f0b1ee2b
“2014005096” <iuwios@hanyang.ac.kr>
2020-05-01 06:25:36 +0900

    Initial commit

---
+++ CrimsonProVariableFont/main.js
@@ -0,0 +1,27 @@
+function intro(){
+	document.getElementById("intro").style.color='#FEC0F0';
+	document.getElementById("design").style.color='white';
+	document.getElementById("example").style.color='white';
+	document.getElementById("recognition").style.color='white';
+}
+
+function design(){
+	document.getElementById("design").style.color='#FEC0F0';
+	document.getElementById("intro").style.color='white';
+	document.getElementById("example").style.color='white';
+	document.getElementById("recognition").style.color='white';
+}
+
+function example(){
+	document.getElementById("example").style.color='#FEC0F0';
+	document.getElementById("design").style.color='white';
+	document.getElementById("intro").style.color='white';
+	document.getElementById("recognition").style.color='white';
+}
+
+function recognition(){
+	document.getElementById("recognition").style.color='#FEC0F0';
+	document.getElementById("design").style.color='white';
+	document.getElementById("example").style.color='white';
+	document.getElementById("intro").style.color='white';
+}
