0dfbbc3121c961dd9f35b797872261ab5fcb6fbb
“2014005096” <iuwios@hanyang.ac.kr>
2020-05-12 19:41:05 +0900

    Initial commit

--- main.js
+++ main.js
@@ -2050,9 +2050,36 @@ function dragElement(elmnt) {
 	}
 }
 
-dragElement(document.getElementById("book"));
+//var box= document.getElementById("book");
 
 
+
+var img = $('.book');
+
+var offset = img.offset();
+var mouseDown = false;
+function mouse(evt) {
+    if(mouseDown ==true){
+    var center_x = (offset.left) + (img.width() / 2);
+    var center_y = (offset.top) + (img.height() / 2);
+    var mouse_x = evt.pageX;
+    var mouse_y = evt.pageY;
+    var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+    var degree = (radians * (180 / Math.PI) * -1) + 90;
+    img.css('-moz-transform', 'rotate(' + degree + 'deg)');
+    img.css('-webkit-transform', 'rotate(' + degree + 'deg)');
+    img.css('-o-transform', 'rotate(' + degree + 'deg)');
+    img.css('-ms-transform', 'rotate(' + degree + 'deg)');
+    }
+}
+
+img.mousedown(function (e) {
+    mouseDown=true;
+    $(document).mousemove(mouse);
+});
+$(document).mouseup(function (e) {
+    mouseDown = false;
+})
 
 
 
